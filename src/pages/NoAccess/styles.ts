688bdb04b836e2ac11ed8d956cbc71517ed1280b
Cleiton Kiper <cleitonahnerth@gmail.com>
2020-08-23 18:06:34 -0400

    Add NoAccess Page

---
+++ src/pages/NoAccess/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+`;
